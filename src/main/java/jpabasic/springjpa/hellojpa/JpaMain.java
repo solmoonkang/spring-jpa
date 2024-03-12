04ec51af5cc85faf292735b349f3abd63f4cc788
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-12 23:03:42 +0900

    feat: JPQL을 사용해 Member 객체 조회

--- src/main/java/jpabasic/springjpa/hellojpa/JpaMain.java
+++ src/main/java/jpabasic/springjpa/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,11 +15,12 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
+            List<Member> findMember = entityManager.createQuery("select m from Member as m", Member.class)
+                            .getResultList();
 
-            entityManager.persist(member);
+            for (Member member : findMember) {
+                System.out.println("member.name = " + member.getName());
+            }
 
             transaction.commit();
         } catch (Exception e) {
