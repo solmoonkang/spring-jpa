f61c1c54931794b3dca1bc98a88ffa3670c061fe
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-12 22:51:54 +0900

    feat: JPA 동작 확인

--- src/main/java/jpabasic/springjpa/hellojpa/JpaMain.java
+++ src/main/java/jpabasic/springjpa/hellojpa/JpaMain.java
@@ -1,6 +1,15 @@
 package jpabasic.springjpa.hellojpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JpaMain {
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
