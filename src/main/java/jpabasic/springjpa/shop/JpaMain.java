01c47a955deca1acceb0b029932f9b656acb7690
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-13 17:16:53 +0900

    feat: 애플리케이션 실행 메소드 구현

---
+++ src/main/java/jpabasic/springjpa/shop/JpaMain.java
@@ -0,0 +1,26 @@
+package jpabasic.springjpa.shop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("springjpa");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        try {
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
+}
