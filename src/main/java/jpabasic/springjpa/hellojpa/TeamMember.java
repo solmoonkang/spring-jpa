d43e0597f52c6b3ff79f09f38523f87aca5d7efb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-13 17:41:19 +0900

    feat: 팀 회원 엔티티 구현

---
+++ src/main/java/jpabasic/springjpa/hellojpa/TeamMember.java
@@ -0,0 +1,40 @@
+package jpabasic.springjpa.hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class TeamMember {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String name;
+
+    @Column(name = "TEAM_ID")
+    private Long teamId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+}
