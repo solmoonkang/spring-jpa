162abb0b4286d04303408ad74344f2e59abd3b31
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-16 21:50:35 +0900

    feat: 일대일 연관관계를 위해 Locker 엔티티 생성

---
+++ src/main/java/jpabasic/springjpa/hellojpa/Locker.java
@@ -0,0 +1,14 @@
+package jpabasic.springjpa.hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Locker {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+}
