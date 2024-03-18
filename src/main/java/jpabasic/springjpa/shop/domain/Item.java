002cd0022e72253943a5532bdf09ba99a3547ca0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-18 10:49:12 +0900

    feat: 카테고리 엔티티와 다대다 연관관계 매핑하도록 구현

--- src/main/java/jpabasic/springjpa/shop/domain/Item.java
+++ src/main/java/jpabasic/springjpa/shop/domain/Item.java
@@ -1,6 +1,8 @@
 package jpabasic.springjpa.shop.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -16,35 +18,6 @@ public class Item {
 
     private int stockQuantity;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getStockQuantity() {
-        return stockQuantity;
-    }
-
-    public void setStockQuantity(int stockQuantity) {
-        this.stockQuantity = stockQuantity;
-    }
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
