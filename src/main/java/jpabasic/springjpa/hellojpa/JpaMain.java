dfdb5b4f9a2169f28e2e846280cfe7455a8de42d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-19 15:20:29 +0900

    feat: JPQL을 사용한 조회 쿼리 구현

--- src/main/java/jpabasic/springjpa/hellojpa/JpaMain.java
+++ src/main/java/jpabasic/springjpa/hellojpa/JpaMain.java
@@ -15,74 +15,10 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            // SAVE TEAM
-            Team team = new Team();
-            team.setName("teamA");
-            entityManager.persist(team);
+            String jpql = "SELECT m FROM TeamMember m WHERE m.name like '%kim%'";
 
-            // SAVE MEMBER
-            TeamMember teamMember = new TeamMember();
-            teamMember.setName("memberA");
-            // 단방향 연관관계 설정, 참조 저장
-            teamMember.setTeam(team);
-            entityManager.persist(teamMember);
-
-            // FIND MEMBER
-            TeamMember findTeamMember = entityManager.find(TeamMember.class, teamMember.getId());
-
-            // 연관관계가 없다.
-            // Team findTeam = entityManager.find(Team.class, team.getId());
-
-            // 참조를 사용해서 연관관계를 조회
-            Team findTeam = findTeamMember.getTeam();
-
-            entityManager.flush();
-            entityManager.clear();
-
-
-
-
-            // SAVE NEW TEAM B
-            Team teamB = new Team();
-            teamB.setName("teamB");
-            entityManager.persist(teamB);
-
-            // "memberA"에 새로운 teamB 설정
-            teamMember.setTeam(teamB);
-
-            entityManager.flush();
-            entityManager.clear();
-
-
-
-
-            TeamMember findTeamMemberB = entityManager.find(TeamMember.class, teamMember.getId());
-            List<TeamMember> teamMembers = findTeamMemberB.getTeam().getTeamMembers();
-
-            for (TeamMember member : teamMembers) {
-                System.out.println("member = " + member.getName());
-            }
-
-            entityManager.flush();
-            entityManager.clear();
-
-
-
-
-
-            Movie movie = new Movie();
-            movie.setDirector("DIRECTOR");
-            movie.setActor("ACTOR");
-            movie.setName("MOVIE NAME");
-            movie.setPrice(10000);
-
-            entityManager.persist(movie);
-
-            entityManager.flush();
-            entityManager.clear();
-
-            Movie findMovie = entityManager.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
+            List<TeamMember> result = entityManager.createQuery(jpql, TeamMember.class)
+                    .getResultList();
 
             transaction.commit();
         } catch (Exception e) {
