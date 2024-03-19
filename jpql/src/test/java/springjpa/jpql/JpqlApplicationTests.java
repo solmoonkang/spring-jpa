ae62d2c05c846b4b688b5efab61ffb02dba4ec59
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-19 16:06:49 +0900

    feat: shop, jqpl 모듈 생성

---
+++ jpql/src/test/java/springjpa/jpql/JpqlApplicationTests.java
@@ -0,0 +1,13 @@
+package springjpa.jpql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shop/src/test/java/springjpa/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package springjpa.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
