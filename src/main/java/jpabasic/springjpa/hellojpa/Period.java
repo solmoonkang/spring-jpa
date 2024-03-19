f19a9d26a9e8a9a18d37b81e9bc36e9be1a8f10d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-19 12:12:55 +0900

    feat: 출, 퇴근 시간을 정의한 임베디드 타입 설정

---
+++ src/main/java/jpabasic/springjpa/hellojpa/Period.java
@@ -0,0 +1,16 @@
+package jpabasic.springjpa.hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import java.time.LocalDateTime;
+
+@Embeddable
+@Getter @Setter
+public class Period {
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+}
