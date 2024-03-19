849c1839991aab7de459a4f11275f18a1e847af5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-19 16:27:42 +0900

    feat: 주문 엔티티 생성

---
+++ jpql/src/main/java/springjpa/jpql/domain/Order.java
@@ -0,0 +1,23 @@
+package springjpa.jpql.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ORDERS")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private int orderAmount;
+
+    @Embedded
+    private Address address;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Member member;
+}
