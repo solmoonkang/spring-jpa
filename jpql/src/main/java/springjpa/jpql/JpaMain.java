e5e39a63627deda78c3f1a2543ed70f55f487cac
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-19 16:27:58 +0900

    feat: 메인 메서드 구현

---
+++ jpql/src/main/java/springjpa/jpql/JpaMain.java
@@ -0,0 +1,27 @@
+package springjpa.jpql;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        try {
+
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
+}
